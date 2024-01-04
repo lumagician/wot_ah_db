26f97bf2f1b4234b1e71a05c60016808b15d5d1b
Leo Hofer <leo.hofer@hofernet.ch>
2024-01-05 00:01:03 +0100

    Add availability_updater.py and data_updater.py scripts

---
+++ availability_updater.py
@@ -0,0 +1,56 @@
+import os
+import hashlib
+import json
+
+# Directory containing geojson files
+export_directory = "exports/"
+
+# create a list of all the available exports
+available_exports = []
+
+# get all the files in the directory
+files = os.listdir(export_directory)
+
+# loop through all the files
+for file in files:
+    # check if the file is a geojson file
+    if file.endswith(".geojson"):
+        # add the file to the available exports list
+        available_exports.append(file)
+        # open the file and read it
+        with open(os.path.join(export_directory, file), 'rb') as f:
+            # read the file
+            data = f.read()
+            # calculate the hash of the file
+            hash_value = hashlib.md5(data).hexdigest()
+            # create a new file with the same name as the original file but with a .hash extension
+            with open(os.path.join(export_directory, file + ".hash"), 'w') as f2:
+                # write the hash to the file
+                f2.write(hash_value)
+
+# create a new list to store export dictionaries
+export_list = []
+
+# go through all the available exports and read the amount of features in each file, the file size, the filename, and the hash
+for export in available_exports:
+    # open the file
+    with open(os.path.join(export_directory, export), 'r') as f2:
+        # read the file
+        data = json.load(f2)
+        # get the amount of features in the file
+        amount_of_features = len(data["features"])
+        # get the file size
+        file_size = os.path.getsize(os.path.join(export_directory, export))
+        # get the hash of the file
+        with open(os.path.join(export_directory, export + ".hash"), 'r') as f3:
+            # read the file
+            hash_value = f3.read()
+            # create a dictionary with the filename, the amount of features, the file size, and the hash
+            export_dict = {"filename": export, "amount_of_features": amount_of_features, "file_size": file_size, "hash": hash_value}
+            # append the dictionary to the list
+            export_list.append(export_dict)
+
+# create a new file called available_exports.json
+with open("available_exports.json", 'w') as f:
+    # write the entire list as a JSON array to the file
+    f.write(json.dumps(export_list, indent=2))
---
+++ data_updater.py
@@ -0,0 +1,50 @@
+# this script uses the overpass api (openstreetmap) to get updated fountain data
+
+import requests
+import json
+import os
+
+AREA_LIST = [
+    ("CH", "Switzerland"),
+    ("DE", "Germany"),
+    ("FR", "France"),
+    ("IT", "Italy"),
+    ("AT", "Austria"),
+    ("LI", "Liechtenstein"),
+]
+
+# create a new directory called exports if it doesn't exist
+if not os.path.exists("exports"):
+    os.makedirs("exports")
+
+
+query_template = """
+[out:json][timeout:25];
+area["ISO3166-1"="{}"][admin_level=2];
+node["amenity"="drinking_water"](area);
+node["amenity"="fountain"](area);
+out center;
+"""
+
+# loop through all the areas
+for area in AREA_LIST:
+    # get the area code and the area name
+    area_code, area_name = area
+    # create a new query using the template and the area code
+    query = query_template.format(area_code)
+    # send the query to the overpass api
+    response = requests.get("http://overpass-api.de/api/interpreter", params={"data": query})
+    # check if the response was successful
+    if response.status_code != 200:
+        # the response was not successful
+        print("Error: Could not get data from overpass api")
+        exit()
+    # restructure the response to be a geojson
+    response = response.json()
+    response = {"type": "FeatureCollection", "features": response["elements"]}
+    # minify the response
+    response = json.dumps(response, separators=(',', ':'))
+    # write the response to a file
+    with open("exports/{}.geojson".format(area_name), 'w') as f:
+        f.write(response)
+    print("Wrote response to file: {}.geojson".format(area_name))
